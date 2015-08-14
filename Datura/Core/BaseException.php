8958ae9c37a9747b22417b7bcfa2536e106ff5cc
Gogs <gogs@fake.local>
2015-08-14 10:08:14 +0800

    异常日志格式调整

--- Datura/Core/BaseException.php
+++ Datura/Core/BaseException.php
@@ -11,9 +11,9 @@ class BaseException extends Exception {
      * 记录异常日志
      */
     protected function _Log() {
+        $err=date('Y-m-d H:i:s') . '|';
         $err = '异常信息：' . self::getMessage() . '|';
-        $err.='异常码：' . self::getCode() . '|';
-        $err.='异常发生时间：' . date('Y-m-d H:i:s') . '|';
+        $err.='异常码：' . self::getCode() . '|';       
         $err.='堆栈回溯:'.serialize(debug_backtrace()) . PHP_EOL;
         file_put_contents(_ROOT . 'Data/exceptionLog.txt', $err, FILE_APPEND);
     }
